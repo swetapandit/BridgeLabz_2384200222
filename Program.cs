3d029ffbb9dff73263241e22b5c8a3a2a0604693
Aapka Naam <aapkaemail@example.com>
2025-02-05 23:59:48 +0530

    [Sweta] Add Assignment of List Question 1

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿/*using System;
+using System.Collections.Generic;
+
+class Book
+{
+    private string title;
+    private string author;
+
+    public Book(string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getter methods
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Book: {GetTitle()}, Author: {GetAuthor()}");
+    }
+}
+
+class Library
+{
+    private string name;
+    private List<Book> books;  // Aggregation: Library ke pass ek List<Book> hai
+
+    public Library(string name)
+    {
+        this.name = name;
+        books = new List<Book>();  // Empty list initialize
+    }
+
+    // Getter method for library name
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);  // Book ko library me add kar rahe hain
+    }
+
+    public void ShowBooks()
+    {
+        Console.WriteLine($"\nLibrary: {GetName()}");
+        foreach (var book in books)
+        {
+            book.Display();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the Book Name ");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Enter the Book Name ");
+        string author = Console.ReadLine();
+
+        Console.WriteLine("Enter the Library Name ");
+        string libraryName = Console.ReadLine();
+
+        // Independent books
+        Book b1 = new Book(title, author);
+        Book b2 = new Book(title, author);
+        Book b3 = new Book(title, author);
+
+        // Creating libraries
+        Library lib1 = new Library(libraryName);
+        Library lib2 = new Library(libraryName);
+
+        // Adding books to different libraries
+        lib1.AddBook(b1);
+        lib1.AddBook(b2);
+        lib2.AddBook(b2);
+        lib2.AddBook(b3);
+
+        // Display books in each library
+        lib1.ShowBooks();
+        lib2.ShowBooks();
+        Console.ReadLine();
+    }
+}
+*/
---
+++ Program1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+class Book1
+{
+    private string title;
+    private string author;
+
+    public Book1(string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public string GetAuthor()
+    {
+        return author;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Book Title: {title}");
+        Console.WriteLine($"Book Author: {author}");
+    }
+}
+
+class Library
+{
+    private string libraryName;
+    private List<Book1> book1s;
+
+    public Library(string libraryName)
+    {
+        this.libraryName = libraryName;
+        book1s = new List<Book1>();
+    }
+
+    public string GetLibraryName()
+    {
+        return libraryName;
+    }
+
+    public void AddBook(Book1 book1)
+    {
+        book1s.Add(book1);
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Displaying data");
+        foreach(var book in book1s)
+        {
+            book.Display();
+        }
+    }
+}
+
+class Program1
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Enter the Book Name: ");
+        //string title = Console.ReadLine();
+
+        //Console.WriteLine("Enter the Book author: ");
+        //string author = Console.ReadLine();
+
+        //Console.WriteLine("Enter the Library Name: ");
+        //string libraryName = Console.ReadLine();
+
+        Book1 book1 = new Book1("Book1", "Author1");
+        Book1 book2 = new Book1("Book2", "Author2");
+        Book1 book3 = new Book1("Book3", "Author3");
+
+        Library lib1 = new Library("Library One");
+        Library lib2 = new Library("Library two");
+
+        lib1.AddBook(book1);
+        lib2.AddBook(book2);
+        lib1.AddBook(book3);
+        lib1.AddBook(book2);
+
+        Console.WriteLine($"{lib1.GetLibraryName()}");
+        lib1.Show();
+        Console.WriteLine($"----------------------------");
+        Console.WriteLine($"{lib2.GetLibraryName()}");
+        lib2.Show();
+        Console.ReadLine();
+    }
+}

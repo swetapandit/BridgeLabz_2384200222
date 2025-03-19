a7e51e9fed57270975d94e792b7c2cc5bcb03dba
Aapka Naam <aapkaemail@example.com>
2025-03-19 20:42:55 +0530

    [Sweta] Add: Added UC1 code to the branch

---
+++ Employee/Server.js
@@ -0,0 +1,8 @@
+const Is_Absent = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == Is_Absent) {
+    console.log("Employee is Absent");
+}
+else {
+    console.log("Employee is Present");
+}
